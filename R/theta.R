e247e32ff3efd4bdef5deb916a4aa6a7c01389c0
Hang Ruan <hang.ruan@hotmail.com>
2016-06-27 09:46:02 +0800

    add theta estimation
    Add a theta estimation function

---
+++ R/theta.R
@@ -0,0 +1,54 @@
+#' @title Estimating theta values from a distance matrix.
+#'
+#' @name esttheta
+#' @rdname theta
+#'
+#' @param disMat a distance matrix with column and row names
+#'
+#' @return returns a vector of theta numbers, each corresponded to a .
+#'
+#' @details
+#'
+#' @export
+#'
+#' @examples
+#' data(tetraexp)
+#' dis.mat <- expdist(tetraexp.objects, taxa = "all",
+#'                      subtaxa = "Brain",
+#'                      method = "rho")
+#' thetas <- esttheta(dis.mat)
+#' hist(thetas)
+#'
+#' @references
+#'
+esttheta = function(disMat = NULL) {
+
+  taxaNames <- row.names(disMat)
+  taxaNumber <- length(taxaNames)
+
+  allCombn <- combn(taxaNumber, 4)
+
+  # for all the combinations  of quartet c(n,4)
+  theta <- vector(mode = "numeric", length = length(allCombn[1,]))
+
+  for (i in 1:length(allCombn[1,])) {
+
+    aCombn <- allCombn[,i]
+
+    aQuartet <- dis.mat[aCombn,aCombn]
+    aQuartet.melted <- melt(aQuartet)
+
+    aQuartet.melted <- aQuartet.melted[aQuartet.melted$value!=0,]
+    sixValues <- aQuartet.melted$value
+
+    threeQuantities <- sort(c(sixValues[3]+sixValues[4],
+                              sixValues[2]+sixValues[5],
+                              sixValues[1]+sixValues[6]))
+
+    theta[i] <- (threeQuantities[3] - threeQuantities[2]) / (threeQuantities[3] - threeQuantities[1])
+
+  }
+
+  theta
+}
+
